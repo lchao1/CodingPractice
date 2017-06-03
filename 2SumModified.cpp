70702ddb6fdd42ff57cdd1ce701f95105dac0597
lchao1 <liangchao.cl@gmail.com>
2017-06-03 16:02:36 -0400

    Finished 2SumModified with triple nest, Kev says there's gotta be a better way haha

--- 2SumModified.cpp
+++ 2SumModified.cpp
@@ -10,19 +10,23 @@ using namespace std;
 
 int main(void) {
 
-	int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int sum = 0;
+	int arr[10] = {1, 3, 7, 10, 12, 17, 19, 23, 25, 30};
+	int sum = 0; //sum of two terms
+	int target = 0; //loop through array to check if sum = a term in the array
 
 	for (int i=0; i<10; i++) {
 		for (int j=0; j<9; j++) {
 			if (j != i) {
-				sum = arr[i] + arr[j];
-			}
-			if (sum == target) {
-				cout << arr[i] << " + "<< arr[j] << " is equal to ";
-				cout << sum;
-				cout << ". hah got it bitch";
-				cout << endl;
+				sum = arr[i] + arr[j]; //sum two terms
+				for (int k = 0; k<10; k++) { //check sum against every term in the array
+					target = arr[k];
+					if (sum == target) {
+						cout << arr[i] << " + "<< arr[j] << " is equal to ";
+						cout << sum;
+						cout << ". hah got it bitch";
+						cout << endl;
+					}
+				}
 			}
 		}
 	}
